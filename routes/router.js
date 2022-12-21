795495cfba94871138b4cd6cdcc7e7f8efc7a16e
LordFeexz <anandafiqri1403@gmail.com>
2022-12-21 13:21:06 +0700

    login and register user

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const userRouter = require("./user");
+const authRouter = require("./auth");
 
 router.use("/user", userRouter);
+
+router.use("/auth", authRouter);
 
 module.exports = router;
