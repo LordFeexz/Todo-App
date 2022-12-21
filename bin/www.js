a6344cd16d60911fc4310f21199689ff4f574607
LordFeexz <anandafiqri1403@gmail.com>
2022-12-21 12:54:12 +0700

    config

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,11 @@
+const { mongoConnect } = require("../config/mongo");
+
 const app = require("../app");
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`example app listening on port ${port}`));
+mongoConnect()
+  .then(() =>
+    app.listen(port, () => console.log(`app listening on port ${port}`))
+  )
+  .catch((err) => console.log(err));
